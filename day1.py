26676c612e1fa8ac9eadbf094852352f032db038
Muhammad361738 <ullahehsan016@gmail.com>
2025-02-02 05:46:46 -0800

    Add new journey with python

--- day1.py
+++ day1.py
@@ -5,3 +5,20 @@ Personal Message: Store a person’s name in a variable, and print a message to 
 """
 personName = "Talha"
 print(F'Hello {personName} would you like to learn some python today ')
+"""
+Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+"""
+myPerson = "Muhammad Talha"
+print(myPerson.upper())
+print(myPerson.lower())
+print(myPerson.title())
+
+"""
+Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. 
+Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed
+. Then print the name after striping the white spaces.
+"""
+myName = "\t Muhammad     Talha        "
+print(myName)
+print(myName.strip())
+print(myName.strip().replace(" ",""))
