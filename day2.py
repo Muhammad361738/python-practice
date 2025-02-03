14e8b79993f616cbb621cf27da9e9ce1281273a9
Muhammad361738 <ullahehsan016@gmail.com>
2025-02-03 05:07:11 -0800

    Add new questions of array and accessing element in it

--- day2.py
+++ day2.py
@@ -6,3 +6,13 @@ print(4+4)
 print(12-4)
 print(4*2)
 print(16/2)
+
+"""
+Names: Store the names of a few of your friends in a array called names.
+ Print each person’s name by accessing each element in the list, one at a time
+"""
+myFriends = ["Waqar", "Salman","Zain","Talha"]
+print(myFriends[0])
+print(myFriends[1])
+print(myFriends[2])
+print(myFriends[3])
