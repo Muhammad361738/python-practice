17b8426a499d6ef50a5a206a70cd5d7945944956
Muhammad361738 <ullahehsan016@gmail.com>
2025-02-03 05:24:40 -0800

    Add new questions of operators

--- day2.py
+++ day2.py
@@ -16,3 +16,24 @@ print(myFriends[0])
 print(myFriends[1])
 print(myFriends[2])
 print(myFriends[3])
+
+"""
+Greetings: Start with the array you used in Exercise 11, but instead of just printing each 
+person’s name, print a message to them. The text of each message should be the same,
+ but each message should be personalized with the person’s name.
+"""
+print(f'Hello {myFriends[0]}')
+print(f'Hello {myFriends[1]}')
+print(f'Hello {myFriends[2]}')
+print(f'Hello {myFriends[3]}')
+
+"""
+Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, 
+and make a list that stores several examples. Use your list to print a series of statements about 
+these items, such as “I would like to own a Honda motorcycle.”
+"""
+
+transportation = ["Honda motorcycle", "Tesla car", "Yamaha bike", "BMW car", "Ducati bike"]
+
+for i in transportation :
+    print(f'I would like to own {i}')
