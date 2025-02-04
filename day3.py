6f2cc56f6bd419d8dbdc592c74b9a50764359489
Muhammad361738 <ullahehsan016@gmail.com>
2025-02-04 06:48:37 -0800

    Add new day in this journey

--- day3.py
+++ day3.py
@@ -7,3 +7,11 @@ Then use your list to print a message to each person, inviting them to dinner.
 myList = ["Waqar","Salman","Zain"]
 for i in myList:
     print(f'{i} I invite you to dinner with me ')
+
+"""
+Changing Guest List: You just heard that one of your guests can’t make the dinner, 
+so you need to send out a new set of invitations. You’ll have to think of someone else to invite
+"""
+myList2 = ["Waqar","Salman","Talha"]
+for i in myList:
+    print(f'{i} I invite you to dinner with me ')
