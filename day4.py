8baed2380a27ce9be3e7d8921437963dad31da87
Muhammad361738 <ullahehsan016@gmail.com>
2025-02-05 08:55:31 -0800

    Add new day in this journey

--- day4.py
+++ day4.py
@@ -3,3 +3,20 @@ Changing Guest List: You just heard that one of your guests can’t make the din
 
 
 """
+
+"""
+• Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+
+• Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+
+• Print a second set of invitation messages, one for each person who is still in your list.
+
+More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+• Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
+
+• Add one new guest to the beginning of your array.
+
+• Add one new guest to the middle of your array. • Use append() to add one new guest to the end of your list. • Print a new set of invitation messages, one for each person in your list.
+
+
+"""
