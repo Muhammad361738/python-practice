12bb4ce05ac937102bc377eb564ed200115dc466
Muhammad361738 <ullahehsan016@gmail.com>
2025-02-25 21:55:55 +0500

    Add new task in python practice

---
+++ day6.py
@@ -0,0 +1,8 @@
+"""
+Write a Python program to print "Hello, World!".
+Create a variable to store your name, age, and city, then print them.
+Take two numbers as input and print their sum, difference, product, and quotient.
+Check whether a given number is even or odd.
+Write a program to swap two numbers without using a third variable.
+
+"""
