7f24061f966a6534be6c53bdc34d6e1e01792286
Muhammad361738 <ullahehsan016@gmail.com>
2025-02-25 22:00:03 +0500

    Add new task in python practice

--- day6.py
+++ day6.py
@@ -6,3 +6,9 @@ Check whether a given number is even or odd.
 Write a program to swap two numbers without using a third variable.
 
 """
+
+# my_name="Zain ali"
+# age=20
+# my_city="karachi"
+# print(my_name, "\n" ,age ,"\n",my_city)
+
